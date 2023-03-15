30fe74024ab61ecb94396fcd3adc4d6976c7ea45
Pavel <rudnitskiypavel20@gmail.com>
2023-03-15 22:27:19 +0200

    fix starting of server

--- index.ts
+++ index.ts
@@ -1,10 +1,7 @@
-const http = require("http");
-const app = require("./app");
-const server = http.createServer(app);
+import app from './app';
 
-const { API_PORT } = process.env;
-const port = process.env.PORT || API_PORT;
+const port = process.env.API_PORT;
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
